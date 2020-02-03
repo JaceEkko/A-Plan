5575c606cd78cd7498aa27cb8ec583c070d2a85d
John Knox <jgk305@nyu.edu>
2020-02-03 08:13:56 -0500

    updated algorithim for time conflict

--- AlgorithimDesign/ScheduleCreationDraft.py
+++ AlgorithimDesign/ScheduleCreationDraft.py
@@ -1,37 +1,39 @@
-class Queue:
+A = [0,0,1,0] #B = [0,1,1,0]
+B = [0,1,1,0]
+C = [1,0,1,0] #D = [0,0,0,0]
+D = [0,0,0,0]
+E = [0,1,0,0] #D = [0,1,0,0]
+F = [0,1,0,0]
 
-  #Constructor creates a list
-  def __init__(self):
-      self.queue = list()
+Times = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-  #Adding elements to queue
-  def enqueue(self,data):
-      #Checking to avoid duplicate entry (not mandatory)
-      if data not in self.queue:
-          self.queue.insert(0,data)
-          return True
-      return False
+C1 = [A,B]
+C2 = [C,D]
+C3 = [E,F]
+CSch = [[0,0,0,0], [0,0,0,0]]
 
-  #Removing the last element from the queue
-  def dequeue(self):
-      if len(self.queue)>0:
-          return self.queue.pop()
-      return ("Queue Empty!")
+def time_conflict(a, b):
+  if a + b > 1:
+    return True
+  else:
+    return False
 
-  #Getting the size of the queue
-  def size(self):
-      return len(self.queue)
+def fill_schedule(course):
+  for i in range(0, 2):
+    for j in range(0, 4):
+      print("i,j: ",i," ",j)
+      if time_conflict(CSch[i][j], course[i][j]) == False:
+        CSch[i][j] += course[i][j]
+      else:
+        print("ErrorLog: Time Conflict")
 
-  #printing the elements of the queue
-  def printQueue(self):
-      return self.queue
-
-
-classList = [
-        {
-            "priority" : 1,
-            "className" : "CS",
-            ""
-            
-            }
-    ]
+print("Course1: ", C1)
+print("Current Schedule: ", CSch);
+fill_schedule(C1)
+print("Course1: ", C2)
+print("Current Schedule: ", CSch);
+fill_schedule(C2)
+print("Course1: ", C3)
+print("Current Schedule: ", CSch);
+fill_schedule(C3)
+print("Final Schedule: ", CSch);
